52ea26216772519b38996d483ef4f1a60366792b
htmldiana <dianekhomenko@gmail.com>
2023-03-29 20:29:13 +0200

    search base

--- src/js/api.js
+++ src/js/api.js
@@ -1,4 +1,31 @@
-import axios from "axios";
+import axios from 'axios';
+import Notiflix from 'notiflix';
 
 const API_KEY = '959330b1b48c95e1fde96a992bbede29';
 const URL = 'https://api.themoviedb.org/';
+
+const refs = {
+  form: document.querySelector('form'),
+  input: document.querySelector('input'),
+};
+
+const onSearch = async e => {
+  Notiflix.Loading.circle();
+  try {
+    const response = await axios.get(
+      `${URL}/3/search/movie?api_key=${API_KEY}&query=${refs.input.value}`
+    );
+    console.log(response.data);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    Notiflix.Loading.remove();
+  }
+};
+
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+  refs.input.value == ''
+    ? Notiflix.Notify.failure('Please enter a keyword')
+    : onSearch();
+});
