368528a39c175e2ed1e46c2b2c686b34f322d013
htmldiana <dianekhomenko@gmail.com>
2023-03-31 17:56:35 +0200

    search with new header

--- src/js/api.js
+++ src/js/api.js
@@ -5,26 +5,35 @@ import FetchFilms from './fetch-films';
 const API_KEY = '959330b1b48c95e1fde96a992bbede29';
 const URL = 'https://api.themoviedb.org/';
 
-// const refs = {
-//   form: document.querySelector('form'),
-//   input: document.querySelector('input'),
-// };
+const refs = {
+  form: document.querySelector('.js-header__form'),
+  input: document.querySelector('.js-header__input'),
+  inputError: document.querySelector('.header__js-input-error'),
+};
 
-// const onSearch = () => {
-//   Notiflix.Loading.circle();
-//   const search = new FetchFilms(
-//     `${URL}/3/search/movie?api_key=${API_KEY}&query=${refs.input.value}`,
-//     document.querySelector('.js-cards-list')
-//   );
-//   document.querySelector('.js-cards-list').innerHTML = '';
-//   search.getFilms();
-//   Notiflix.Loading.remove();
-// };
+const onSearch = () => {
+  Notiflix.Loading.circle();
+  refs.inputError.classList.add('visually-hidden');
 
-// // event listener for search input, will be changed
-// refs.form.addEventListener('submit', e => {
-//   e.preventDefault();
-//   refs.input.value == ''
-//     ? Notiflix.Notify.failure('Please enter a keyword')
-//     : onSearch();
-// });
+  const search = new FetchFilms(
+    `${URL}/3/search/movie?api_key=${API_KEY}&query=${refs.input.value}`,
+    document.querySelector('.js-cards-list')
+  );
+
+  document.querySelector('.js-cards-list').innerHTML = '';
+
+  search.getFilms();
+
+  Notiflix.Loading.remove();
+
+  if (document.querySelector('.js-cards-list').innerHTML == '') {
+    refs.inputError.classList.remove('visually-hidden');
+  }
+};
+
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+  refs.input.value == ''
+    ? refs.inputError.classList.remove('visually-hidden')
+    : onSearch();
+});
