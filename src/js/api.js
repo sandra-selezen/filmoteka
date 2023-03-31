63debcbe948f1442ced06b59a5aaea8ee1243f60
htmldiana <dianekhomenko@gmail.com>
2023-03-31 17:11:41 +0200

    render on search

--- src/js/api.js
+++ src/js/api.js
@@ -1,41 +1,30 @@
-import axios from "axios";
+import axios from 'axios';
 import Notiflix from 'notiflix';
+import FetchFilms from './fetch-films';
 
 const API_KEY = '959330b1b48c95e1fde96a992bbede29';
 const URL = 'https://api.themoviedb.org/';
 
-/*
-// refs for keysearch, will be changed
-const refs = {
-  form: document.querySelector('form'),
-  input: document.querySelector('input'),
-};
-*/
+// const refs = {
+//   form: document.querySelector('form'),
+//   input: document.querySelector('input'),
+// };
 
-/*
-// function for keyword search, will be changed
+// const onSearch = () => {
+//   Notiflix.Loading.circle();
+//   const search = new FetchFilms(
+//     `${URL}/3/search/movie?api_key=${API_KEY}&query=${refs.input.value}`,
+//     document.querySelector('.js-cards-list')
+//   );
+//   document.querySelector('.js-cards-list').innerHTML = '';
+//   search.getFilms();
+//   Notiflix.Loading.remove();
+// };
 
-const onSearch = async e => {
-  Notiflix.Loading.circle();
-  try {
-    const response = await axios.get(
-      `${URL}/3/search/movie?api_key=${API_KEY}&query=${refs.input.value}`
-    );
-    console.log(response.data);
-  } catch (error) {
-    console.log(error);
-  } finally {
-    Notiflix.Loading.remove();
-  }
-};
-*/
-
-/*
-// event listener for search input, will be changed
-refs.form.addEventListener('submit', e => {
-  e.preventDefault();
-  refs.input.value == ''
-    ? Notiflix.Notify.failure('Please enter a keyword')
-    : onSearch();
-});
-*/
+// // event listener for search input, will be changed
+// refs.form.addEventListener('submit', e => {
+//   e.preventDefault();
+//   refs.input.value == ''
+//     ? Notiflix.Notify.failure('Please enter a keyword')
+//     : onSearch();
+// });
