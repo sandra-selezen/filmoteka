d7e283f440332b8aad9ef55bdfed58a392d31c7e
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2023-03-31 22:25:00 +0300

    створив запит на бекенд за інфо про фільм

--- src/js/fetch-one-film.js
+++ src/js/fetch-one-film.js
@@ -1,7 +1,11 @@
+import axios from 'axios';
+
 const listMovies = document.querySelector('.js-cards-list');
 const btnCloseModalMovie = document.querySelector('.js-btn-close-modal');
 const cardMovie = document.querySelector('.js-modal-card');
 const backdropMovie = document.querySelector('.js-backdrop-movie');
+
+//-----слухачі подій-----
 
 listMovies.addEventListener('click', onOpenModalMovieClick);
 btnCloseModalMovie.addEventListener('click', onCloseModalClick);
@@ -18,20 +22,38 @@ backdropMovie.addEventListener('click', event => {
   }
 });
 
-function onOpenModalMovieClick(params) {
+//-----функції------
+
+async function onOpenModalMovieClick(event) {
+  // показую модалку
   backdropMovie.classList.remove('is-hidden');
+  // отримую id конкретного фільму
+  const perentNodLi = event.target.closest('li');
+  const idMovie = perentNodLi.dataset.id;
+  
+  const response = await fetchData('124046');
+  console.log(response.data);
 }
 
 function onCloseModalClick() {
   backdropMovie.classList.add('is-hidden');
 }
 
-function selectColor(event) {
-  if (event.target.nodeName !== 'BUTTON') {
-    return;
+// функція запиту на бекенд
+async function fetchData(idMovie) {
+  const API_URL = `https://api.themoviedb.org/3/movie/${idMovie}`;
+  // параметри запиту на бекенд
+  const options = {
+    params: {
+      api_key: '959330b1b48c95e1fde96a992bbede29',
+      language: 'en-US',
+    },
+  };
+  try {
+    const response = await axios.get(API_URL, options);
+    // console.log(response);
+    return response;
+  } catch (error) {
+    console.log(error);
   }
-
-  const selectedColor = event.target.dataset.color;
-  output.textContent = `Selected color: ${selectedColor}`;
-  output.style.color = selectedColor;
 }
