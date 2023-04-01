12dcfbc8a3d3957ef59903901b76170bbd728ac3
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2023-04-01 15:59:14 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './js/api';
-import './js/popular-films-fetch';import './js/popular-films-fetch';
+import './js/popular-films-fetch';
+import './js/popular-films-fetch';
 import './js/teem-modal';
 import './js/fetch-one-film';
