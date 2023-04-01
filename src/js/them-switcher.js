1665f4ed9e20ac3a97a1d2473be9d69130dd60f1
Mark Zemlianskyi <mark080501@gmail.com>
2023-04-01 19:39:27 +0200

    script theme change

---
+++ src/js/them-switcher.js
@@ -0,0 +1,9 @@
+const themSwitcherFunction = () => {
+  const rootElement = document.documentElement;
+  let dataAtribute = rootElement.getAttribute('data-theme');
+  let newTheme = dataAtribute === 'light' ? 'dark' : 'light';
+  rootElement.setAttribute('data-theme', newTheme);
+};
+document
+  .querySelector('.theme-switcher')
+  .addEventListener('click', themSwitcherFunction);
