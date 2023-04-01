50bf89d83b0ccc7c84820170b02cd535700b05f9
AntonPetrenko <paa2017sh@gmail.com>
2023-04-01 19:36:02 +0300

    update popular-films-fetch.js

--- src/js/popular-films-fetch.js
+++ src/js/popular-films-fetch.js
@@ -1,17 +1,21 @@
 import FetchFilms from './fetch-films';
-// import { nextPage } from './api';
+import { settingsPagination } from './pagination';
 
-let nextPage = 3;
+let nextPage = 1;
 const API_KEY = '959330b1b48c95e1fde96a992bbede29';
 const URL = 'https://api.themoviedb.org/';
 
-export const fetchPopularFilms = (nextPage) => {
+export const fetchPopularFilms = (nextPage=1) => {
  const fetchFilms = new FetchFilms(
  `${URL}/3/trending/movie/day?api_key=${API_KEY}&page=${nextPage}`,
   document.querySelector('.js-cards-list')
 );
-
-fetchFilms.getFilms();
+  try {
+    fetchFilms.getFilms();
+    settingsPagination.requestType = 'popularFilm';
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 fetchPopularFilms(nextPage);
