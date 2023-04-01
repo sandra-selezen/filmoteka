9b0bfbf7071930112a0f991aa0e1685f36654483
Mark Zemlianskyi <mark080501@gmail.com>
2023-04-01 19:40:24 +0200

    save thame atribute to localStorage

--- src/js/them-switcher.js
+++ src/js/them-switcher.js
@@ -3,6 +3,7 @@ const themSwitcherFunction = () => {
   let dataAtribute = rootElement.getAttribute('data-theme');
   let newTheme = dataAtribute === 'light' ? 'dark' : 'light';
   rootElement.setAttribute('data-theme', newTheme);
+  localStorage.setItem('theme', newTheme);
 };
 document
   .querySelector('.theme-switcher')
