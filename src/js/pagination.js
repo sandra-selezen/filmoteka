190cd30d1b843b89e2daa3bb6fbb30393352c6a5
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-04-03 21:00:05 +0300

    Update pagination.js

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,5 +1,5 @@
 import Pagination from 'tui-pagination';
-import 'tui-pagination/dist/tui-pagination.css';
+// import 'tui-pagination/dist/tui-pagination.css';
 
 
 export default class CreatePagination {
